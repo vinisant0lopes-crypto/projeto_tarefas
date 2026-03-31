5c68bd28d6976f041d11a7199d8fe822a3802aa0
vinisant0lopes-crypto <vinisant0lopes@gmail.com>
2026-03-31 17:24:02 -0300

    atualizacao nova tarefa

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+
+  const tarefas =[
+    {id:"1",titulo:"aprender Git"},
+    {id:"2",titulo:"aprender fazer commit"},
+    {id:"3",titulo:"aprender utilizar GitHub"},
+    {id:"3",titulo:"Criar um novo commit"},
+
+  ]
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.titulo}>lista de tarefas</Text>
+      
+ 
+
+   <FlatList
+   data={tarefas}
+   keyExtractor={(item)=>item.id}
+   renderItem={({item})=>(
+    <text style={styles.items}>{item.titulo}</text>
+
+   )}
+
+   
+   
+   />
     </View>
   );
 }
 
+// Estilos utilizando StyleSheet.create para melhor performance
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d71111',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
+  titulo:{
+    fontSize:48,
+    fontWeight:'bold',
+    color:'black',
+    marginBottom:20,
+  },
+  items:{
+    color:'black',
+    padding:15,
+    marginTop:10,
+    backgroundColor:'#e82a2a',
+    borderRadius:5
+  }
+
+
 });
+
